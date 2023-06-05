7f1f94f471c9e6e7819acdab52678dbd32967720
Priyansh Parikh <Priyansh.Parikh@ibm.com>
2023-06-06 00:26:50 +0530

    changes for new

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const http= require('http');
-
-// function rqListener(req,res) {
-// };
-
+// function rqListener(req,res) {};
 // http.createServer(rqListener);
 
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    console.log(req.url,req.method,req.headers);
     // process.exit();
 });
-
 server.listen(3000);
+// console.log(server.address().port);
