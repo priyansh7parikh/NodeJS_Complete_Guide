2858d25f61306b8d1e38647eae8c72436d30c896
Priyansh Parikh <Priyansh.Parikh@ibm.com>
2023-06-13 22:55:28 +0530

    Updates with body-parser and app.js using express

--- Day1_Express/app.js
+++ Day1_Express/app.js
@@ -1,23 +1,21 @@
 const express = require('express');
-
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use('/',(req,res,next)=>{
-    console.log('This Always runs');
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product',(req,res,next)=>{
     console.log("Add-Product")
-    res.send('<form action="/product" method="POST"><input type="text" name ="title"><button type="Submit"></button></form>')
+    res.send('<form action="/product" method="POST"><input type="text" name ="title"><button type="submit">Add Product</button></form>')
 });
 
-app.use('/product',(req,res,next)=>{
+app.post('/product',(req,res,next)=>{
     console.log(req.body);
     res.redirect('/');
 })
 
 app.use('/',(req,res,next)=>{
-    
+    console.log('In another middleware');
+    res.send('<h1>Homepage</h1>')
 })
 app.listen(3000);
