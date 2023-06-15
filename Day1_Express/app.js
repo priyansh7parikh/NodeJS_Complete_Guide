648a0c9d905efbc3685fe8c132d293889c14ff7c
Priyansh Parikh <Priyansh.Parikh@ibm.com>
2023-06-16 01:56:33 +0530

    Added util folder for path

--- Day1_Express/app.js
+++ Day1_Express/app.js
@@ -6,6 +6,7 @@ const app = express();
 
 const adminRoutes = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
+const rootDir = require('./util/path.js');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -14,7 +15,9 @@ app.use(shopRoutes);
 
 app.use((req,res,next)=>{
     // res.status(404).send('<h1>Error Page Not Found</h1>');
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
+    // const fp = path.join(rootDir,'views','404.html');
+    // console.log(fp);
+    res.status(404).sendFile(path.join(rootDir,'views','404.html'))
 })
 
 app.listen(3000);
